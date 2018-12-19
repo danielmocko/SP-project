edd4b755ebf027a95e646fe2a8b41bc1052f31eb
danielmocko <danielmocko50@gmail.com>
2018-12-19 09:51:38 +0100

    App Send message and open the Maps

--- app/src/main/java/com/example/mocko/someprojectandroid/MainActivity.java
+++ app/src/main/java/com/example/mocko/someprojectandroid/MainActivity.java
@@ -17,129 +17,53 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private static final String TAG = MainActivity.class.getSimpleName();
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
 
-    /**
-     * Creates the activity, sets the view, and checks for SMS permission.
-     *
-     * @param savedInstanceState Instance state
-     */
+    // Kreira activity, postavlja view, poziva metodu checkForSmsPermission() za SMS permisiju
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Check to see if SMS is enabled.
         checkForSmsPermission();
     }
 
-    /**
-     * Checks whether the app has SMS permission.
-     */
+    //Metoda koja proverava da li je prihvacena perimisja(dozvola) za SMS
     private void checkForSmsPermission() {
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-            Log.d(TAG, getString(R.string.permission_not_granted));
-            // Permission not yet granted. Use requestPermissions().
-            // MY_PERMISSIONS_REQUEST_SEND_SMS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.SEND_SMS},
-                    MY_PERMISSIONS_REQUEST_SEND_SMS);
-        } else {
-            // Permission already granted. Enable the SMS button.
-            enableSmsButton();
+        //Proverava da li je permisija odobrena- ako nije onda se ulazi u IF selekciju
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            // Poziva dijalog koji se pojavljuje na uredjaju preko kojeg korisnik prihvata permisiju
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
         }
     }
 
-    /**
-     * Processes permission request codes.
-     *
-     * @param requestCode  The request code passed in requestPermissions()
-     * @param permissions  The requested permissions. Never null.
-     * @param grantResults The grant results for the corresponding permissions
-     *                     which is either PERMISSION_GRANTED or PERMISSION_DENIED. Never null.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        // For the requestCode, check if permission was granted or not.
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
-                if (permissions[0].equalsIgnoreCase(Manifest.permission.SEND_SMS)
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Permission was granted. Enable sms button.
-                    enableSmsButton();
-                } else {
-                    // Permission denied.
-                    Log.d(TAG, getString(R.string.failure_permission));
-                    Toast.makeText(this, getString(R.string.failure_permission),
-                            Toast.LENGTH_LONG).show();
-                    // Disable the sms button.
-                    disableSmsButton();
-                }
-            }
-        }
-    }
-
-    /**
-     * Defines a string (destinationAddress) for the phone number
-     * and gets the input text for the SMS message.
-     * Uses SmsManager.sendTextMessage to send the message.
-     * Before sending, checks to see if permission is granted.
-     *
-     * @param view View (message_icon) that was clicked.
-     */
+    //Metoda koja se poziva kada u activity_main klikne na ikonicu za slanje poruke
     public void smsSendMessage(View view) {
-        EditText editText = (EditText) findViewById(R.id.editText_main);
-        // Set the destination phone number to the string in editText.
-        String destinationAddress = editText.getText().toString();
-        // Find the sms_message view.
-        EditText smsEditText = (EditText) findViewById(R.id.sms_message);
-        // Get the text of the sms message.
-        String smsMessage = smsEditText.getText().toString();
-        // Set the service center address if needed, otherwise null.
+        //Kreira se referenca na editText_main u activity_main za unos telefonskog broja
+        EditText phoneNumber = (EditText) findViewById(R.id.editText_main);
+        //U string destinationAddress se pokupi uneti broj iz phoneNumber
+        String destinationAddress = phoneNumber.getText().toString();
+        //Kreira se smsBody koji refencira sms_message za unos koordinata
+        EditText smsBody = (EditText) findViewById(R.id.sms_message);
+        //U string smsMessage se sadrzaj iz smsBody
+        String smsMessage = smsBody.getText().toString();
+
         String scAddress = null;
-        // Set pending intents to broadcast
-        // when message sent and when delivered, or set to null.
+
         PendingIntent sentIntent = null, deliveryIntent = null;
-        // Check for permission first.
+        //Poziv metoda za proveru permisije
         checkForSmsPermission();
-        // Use SmsManager.
+
+        //Kreiranje smsManager objekta za slanje poruke na drugi broj(u ovom slucaju na broj koji je u GPS lokatoru)
         SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(destinationAddress, scAddress, smsMessage,
-                sentIntent, deliveryIntent);
-    }
-
-    /**
-     * Makes the sms button (message icon) invisible so that it can't be used,
-     * and makes the Retry button visible.
-     */
-    private void disableSmsButton() {
-        Toast.makeText(this, R.string.sms_disabled, Toast.LENGTH_LONG).show();
-        ImageButton smsButton = (ImageButton) findViewById(R.id.message_icon);
-        smsButton.setVisibility(View.INVISIBLE);
-        Button retryButton = (Button) findViewById(R.id.button_retry);
-        retryButton.setVisibility(View.VISIBLE);
-    }
-
-    /**
-     * Makes the sms button (message icon) visible so that it can be used.
-     */
-    private void enableSmsButton() {
-        ImageButton smsButton = (ImageButton) findViewById(R.id.message_icon);
-        smsButton.setVisibility(View.VISIBLE);
-    }
-
-    /**
-     * Sends an intent to start the activity.
-     *
-     * @param view  View (Retry button) that was clicked.
-     */
-    public void retryApp(View view) {
-        Intent intent = getPackageManager().getLaunchIntentForPackage(getPackageName());
-        startActivity(intent);
+        /*
+        Poziv metode za slanje SMS poruke na GPS lokator
+        @param destinationAddress :Boj telefona koji je pokupljen iz phoneNumber
+        @param scAddress :Prosledjuje se null vrednost da bi se koristila default vrednost SMSC
+                    SMSC vrednost je default vrednost u sistemskoj aplikaciji SMS-a
+        @param smsMesssage :Za slanje sadrzaja poruke
+        @param sentIntent :Objekat PendingIntent koji se koristi za proveru da li je poruka uspesno poslata
+        @param deliveryIntent :Objekat PendingIntent koji se koristi za proveru da li je poruka uspesno isporucena
+        */
+        smsManager.sendTextMessage(destinationAddress, scAddress, smsMessage, sentIntent, deliveryIntent);
     }
 }
